158bb1da010ce8aa313e5e872a1fd2325b3dbfeb
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2024-07-31 13:38:29 +0300

    Adding funcFeatureB

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 def basicFunction(arg1):
     print(f"This is a basicFunction and it got {arg1} as an argument")
     print(f"The type of this argument is {type(arg1)}")
+
+def funcFeatureB(arg1, arg2, arg3):
+    print("It is a funcFeatureB call")
