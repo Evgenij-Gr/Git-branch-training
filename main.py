90eca404f1903eef3875199b4cc3aba73c3a24d1
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2024-07-31 13:49:02 +0300

    New function on the main branch

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 def basicFunction(arg1):
     print(f"This is a basicFunction and it got {arg1} as an argument")
     print(f"The type of this argument is {type(arg1)}")
+
+def newFuncMain(arg1):
+    print(f"This is an amazing new feature on the main branch")
+    print(f"This function got argument {arg1} of type {type(arg1)}")
