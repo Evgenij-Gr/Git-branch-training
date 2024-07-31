f7ce29ca870d376f4df208a54fd2e487e3021980
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2024-07-31 13:30:46 +0300

    Adding funcFeatureA

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 def basicFunction(arg1):
     print(f"This is a basicFunction and it got {arg1} as an argument")
+
+def funcFeatureA(arg1):
+    print(f"This is a funcFeatureA and it got {arg1} as an argument")
