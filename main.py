9e134c916ea3207b6e665c39a03a4e00cc5c704a
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2024-07-31 13:34:07 +0300

    Improving funcFeatureA further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def basicFunction(arg1):
     print(f"This is a basicFunction and it got {arg1} as an argument")
 
-def funcFeatureA(arg1):
-    print(f"This is a funcFeatureA and it got {arg1} as an argument")
-    print(f"Also argument has type {type(arg1)}")
+def funcFeatureA(arg1, arg2):
+    print(f"This is a funcFeatureA and it got {arg1} and {arg2} as arguments")
+    print(f"Also arguments have type {type(arg1)} and {type(arg2)}")
