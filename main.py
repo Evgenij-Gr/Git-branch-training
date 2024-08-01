7445d92b694e5ce9efee51d276300b9bfdf025ea
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2024-08-01 12:44:00 +0300

    Introducing funcNewFeatureA that we do not want to backport to main branch

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ def basicFunction(arg1):
 def funcFeatureA(arg1, arg2):
     print(f"This is a funcFeatureA and it got {arg1} and {arg2} as arguments")
     print(f"Also arguments have type {type(arg1)} and {type(arg2)}")
+    print(f"This is also a feature that we want to backport to main branch")
+
+def funcNewFeatureA(arg1, arg2):
+    print(f"This is a funcFeatureA and it got {arg1} and {arg2} as arguments")
+    print(f"Also arguments have type {type(arg1)} and {type(arg2)}")
+    print(f"This is also a feature that we do not want to backport to main branch")
