61e07fb20f8030645e53c440451124501329ea60
Evgenij-Gr <Evgenij-Gr@users.noreply.github.com>
2024-08-01 13:39:03 +0300

    Another new feature in branch FeatureA

--- main.py
+++ main.py
@@ -10,3 +10,6 @@ def funcNewFeatureA(arg1, arg2):
     print(f"This is a funcFeatureA and it got {arg1} and {arg2} as arguments")
     print(f"Also arguments have type {type(arg1)} and {type(arg2)}")
     print(f"This is also a feature that we do not want to backport to main branch")
+
+def funcAnotherNewFeatureA():
+    print("FeatureA branch went even further in development since diverging")
